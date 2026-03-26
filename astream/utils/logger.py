8e574372beb74ee5f58aa2a520c19f99e43d870c
meylim <sajidtir@gmail.com>
2026-03-26 14:39:34 +0100

    Update logger.py

--- astream/utils/logger.py
+++ astream/utils/logger.py
@@ -25,6 +25,7 @@ def setup_logger():
     logger.level("PROXY", no=37, icon="🌐", color="<fg #00BCD4>")
     logger.level("PERFORMANCE", no=35, icon="⚡", color="<fg #FFEB3B>")
     logger.level("DATASET", no=33, icon="📦", color="<fg #607D8B>")
+    logger.level("ID_RESOLVER", no=36, icon="🔗", color="<fg #FF9800>")
 
     logger.level("INFO", icon="💡", color="<fg #00BCD4>")
     logger.level("DEBUG", icon="🔍", color="<fg #795548>")
@@ -60,3 +61,4 @@ def setup_logger():
 
 
 setup_logger()
+    
